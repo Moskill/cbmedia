4dadbfbe5df981eeb719a8cd0a5150a6409f5451
unknown <cborek@web.de>
2022-02-22 12:22:21 +0100

    Slider fertig gestellt

--- pages/components/body/slider/Slider.jsx
+++ pages/components/body/slider/Slider.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Slider.module.css';
-
+import design from '../../../assets/design.jpg';
+import development from '../../../assets/development.jpg';
+import photographic from '../../../assets/photographic.jpg';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
+import { SliderData } from '../slider/SliderData';
 
 const Slider = (props) => {
-  console.log(props)
+
+  const [current, setCurrent] = useState(0);
+  const length = SliderData.length;
+
+  const getImage = (image) => {
+    switch (image) {
+      case 'design':
+        return design.src;  
+      case 'development':
+        return development.src;  
+      case 'photographic':
+        return photographic.src;  
+    }
+  }
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length -1 : current - 1)
+  }
+
+  console.log(current, 'CURENT')
+
   return (
-    <div>Slider</div>
+    <>
+      <section className={classes.slider}>
+    <div className={classes['slider-infobox']}>
+      <h1>{SliderData[current].title}</h1>
+    </div>
+        <FaArrowAltCircleLeft className={classes['left-arrow']} onClick={prevSlide}/>
+        <FaArrowAltCircleRight className={classes['right-arrow']} onClick={nextSlide}/>
+        {SliderData.map((item, index) => {
+          return (
+            <div className={index === current ? classes['slide-active'] : classes.slide} key={index}>
+              {index === current && (
+                <img src={getImage(item.image)} alt={item.subtitle} className={classes.image}/> 
+              )}
+            </div>
+          )
+        })}
+      
+      </section>
+    </>
   )
 }
 
--- pages/components/body/slider/SliderData.jsx
+++ pages/components/body/slider/SliderData.jsx
@@ -1,23 +1,23 @@
 export const SliderData = [
   {
-    image: 'development.jpg',
+    image: 'development',
     title: 'WEBENTWICKLUNG',
     link: 'webentwicklung',
     subtitle: 'Webdesign- und Entwicklung',
     text: 'Wir machen Ihren Webauftritt zu einem unvergesslichen Nutzererlebnis. Ob Sie eine Landingpage, eine Firmenseite oder nur ein technisches Upgrade Ihrer bestehenden Seite benötigen - wir unterstützen Sie in jeder Hinsicht, damit Ihr Auftritt im Netz individuell und einzigartig bleibt. Rufen Sie noch heute an! '
   },
   {
-    image: 'design.jpg',
+    image: 'design',
     title: 'DESIGN',
     link: 'design',
     subtitle: 'Logo-Design und Mediengestaltung',
     text: 'Sie benötigen ein Logo oder einen Flyer für ein neues Projekt? Oder brauchen Sie vielleicht ein Prospekt für ein neues Produkt? Wir fertigen auf Wunsch auch Webgrafiken, Banner, Galeriebilder, Visitenkarten, Plakate uvm. für Sie an. Kontaktieren Sie uns jetzt!'
   },
   {
-    image: 'photographic.jpg',
+    image: 'photographic',
     title: 'PRODUKTFOTOGRAFIE',
     link: 'photo',
     subtitle: 'Bilder sagen mehr als tausend Worte...',
-    text: 'Mit über 10 Jahren Erfahrung in der Produktfotografie - speziell für eCommerce - wissen wir sehr genau, auf was der Käufer achtet. Wir setzen Ihr Produkte perfekt in Szene und überzeugen auch den kritischten Kunden.'
+    text: 'Mit über 10 Jahren Erfahrung in der Produktfotografie - speziell für eCommerce - wissen wir sehr genau, auf was der Käufer achtet. Wir setzen Ihr Produkte perfekt in Szene und überzeugen auch den kritischsten Kunden.'
   }
 ]
