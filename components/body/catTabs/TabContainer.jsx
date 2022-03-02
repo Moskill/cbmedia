c7daf128f0e9b29b21a4bf3f9a2a4c270dbbfe5c
unknown <cborek@web.de>
2022-03-02 12:52:08 +0100

    Snap Scroll eingebaut und noch weitere Sachen

--- components/body/catTabs/TabContainer.jsx
+++ components/body/catTabs/TabContainer.jsx
@@ -3,15 +3,18 @@ import classes from './TabContainer.module.css';
 import SingleTab from './SingleTab';
 
 const TabContainer = () => {
+
   return (
-    <div className={classes.container}>
-      <SingleTab />
-      <SingleTab />
-      <SingleTab />
-      <SingleTab />
-      <SingleTab />
-      <SingleTab />
-    </div>
+    <>
+      <div className={classes.container}>
+        <SingleTab />
+        <SingleTab />
+        <SingleTab />
+        <SingleTab />
+        <SingleTab />
+        <SingleTab />
+      </div>
+    </>
   )
 }
 
