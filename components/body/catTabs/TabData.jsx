7ec9c37486b799b8a7606c486b96acd1edf640aa
unknown <cborek@web.de>
2022-03-02 23:04:23 +0100

    Alles Mögliche an Kram erledigt

--- components/body/catTabs/TabData.jsx
+++ components/body/catTabs/TabData.jsx
@@ -1,9 +1,44 @@
 export const TabData = [
   {
-    image: 'development',
+    image: 'webentwicklung',
     title: 'WEBENTWICKLUNG',
     link: 'webentwicklung',
     subtitle: 'Webdesign- und Entwicklung',
+    text:'Sie sind auf der Suche nache jemandem, der Ihnen eine Webseite erstellt? Oder Sie haben schon eine Seite, die nur ein wenig aufgehübscht oder modernisiert werden muss? Ob Sie eine kleine Seite für Ihr Unternehmen oder Ihren Verein brauchen, oder einen kompletten Online-Shop - Kommen Sie auf uns zu!'
+  },
+  {
+    image: 'webdesign',
+    title: 'WEBDESIGN',
+    link: 'webdesign',
+    subtitle: 'Webdesign- und Entwicklung',
     text:'Something'
-  }
+  },
+  {
+    image: 'logodesign',
+    title: 'LOGODESIGN',
+    link: 'logodesign',
+    subtitle: 'Design und Erstellung von Logos',
+    text:'Something'
+  },
+  {
+    image: 'produktfotografie',
+    title: 'WEBENTWICKLUNG',
+    link: 'produktfotografie',
+    subtitle: 'Produktfotos für Ihr Angebot',
+    text:'Something'
+  },
+  {
+    image: 'mediendesign',
+    title: 'MEDIENDESIGN',
+    link: 'mediendesign',
+    subtitle: 'Gestaltung versch. Printmedien',
+    text:'Something'
+  },
+  {
+    image: 'consulting',
+    title: 'CONSULTING',
+    link: 'consulting',
+    subtitle: 'Beratung für Ihren Online-Handel',
+    text:'Something'
+  },
 ]
