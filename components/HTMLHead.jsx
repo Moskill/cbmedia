af4e5695e47a397ec3ac5ca71380812ec13c64f9
unknown <cborek@web.de>
2022-03-04 11:18:23 +0100

    Slider für die Referenzen weiter gebaut

--- components/HTMLHead.jsx
+++ components/HTMLHead.jsx
@@ -24,6 +24,12 @@ const HTMLHead = () => {
       <meta name="msapplication-TileColor" content="#ffffff"/>
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
       <meta name="theme-color" content="#ffffff"/>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossorigin="anonymous"
+      />
     </Head>
 
   )
